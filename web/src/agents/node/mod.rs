94506d80a0f888b7ff1a36835d51cb7d56e62899
tharvik <tharvik@users.noreply.github.com>
2021-11-10 15:56:54 +0100

    page/your_account: show txs

--- web/src/agents/node/mod.rs
+++ web/src/agents/node/mod.rs
@@ -1,5 +1,7 @@
-mod send_asset;
 mod get_balance;
+mod get_latest_transactions;
+mod send_asset;
 
-pub use send_asset::SendAsset;
 pub use get_balance::GetBalance;
+pub use get_latest_transactions::GetLatestTransactions;
+pub use send_asset::SendAsset;
