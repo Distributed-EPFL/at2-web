2575aff474c01618d216b973b56de4dc9d60c073
tharvik <tharvik@users.noreply.github.com>
2021-11-29 17:58:28 +0100

    pages: load user from local storage

--- at2-ns/src/contact.rs
+++ at2-ns/src/contact.rs
@@ -2,6 +2,7 @@ use drop::crypto::sign;
 
 /// Other users on the network
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Contact {
     /// Name of this user
     pub name: String,
