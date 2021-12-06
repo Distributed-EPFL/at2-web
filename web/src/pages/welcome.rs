e66426c0e5db8adc89fb514ac5b07a690c82f1da
tharvik <tharvik@users.noreply.github.com>
2021-12-06 16:24:28 +0100

    page/welcome: add links

--- web/src/pages/welcome.rs
+++ web/src/pages/welcome.rs
@@ -22,8 +22,12 @@ impl Component for Welcome {
         html! { <>
             <h1> { "Welcome to AT2" } </h1>
 
-            <p> { "
-                AT2 stands for Asynchronous Trustworthy Transfer, it is a
+            <p>
+                <a href="https://factory.c4dt.org/showcase/at2"> { "AT2" } </a>
+                { " is a research project developed at Prof. Rachid Guerraoui's " }
+                <a href="https://dcl.epfl.ch"> { "Distributed Computing Lab" } </a>
+                { ", at EPFL.
+                It stands for Asynchronous Trustworthy Transfer, it is a
                 new way to transfer assets (such as coins) throughout a
                 network of potentially rogue participants.
                 It is quite close to Bitcoin in its capabilities, while
